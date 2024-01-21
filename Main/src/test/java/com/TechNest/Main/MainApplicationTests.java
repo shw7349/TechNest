9b88ba5a143f4b244b4c4c6c1b062ad47cfcaffd
송현우 <shw7349@naver.com>
2024-01-21 10:44:28 +0900

    2024.01.21 FirstCommit

---
+++ Main/src/test/java/com/TechNest/Main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TechNest.Main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
